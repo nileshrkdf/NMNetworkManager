2359045f8f9be0c49589232bef34e2d232b56879
nileshrkdf <nileshrkdf@gmail.com>
2020-10-25 21:19:42 -0500

    Adding draft version

---
+++ NMNetworkManager/NMNetworkManager.h
@@ -0,0 +1,18 @@
+//
+//  NMNetworkManager.h
+//  NMNetworkManager
+//
+//  Created by Nilesh Malviya on 10/25/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NMNetworkManager.
+FOUNDATION_EXPORT double NMNetworkManagerVersionNumber;
+
+//! Project version string for NMNetworkManager.
+FOUNDATION_EXPORT const unsigned char NMNetworkManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NMNetworkManager/PublicHeader.h>
+
+
---
+++ NMNetworkManager/NetworkManager.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.swift
+//  NMNetworkManager
+//
+//  Created by Nilesh Malviya on 10/25/20.
+//
+
+import Foundation
+
+public class NetworkManager {
+    
+//    static public let shared = NetworkManager()
+    
+    static public func getMethod() {
+        print("Hi from Network Manager")
+    }
+}
